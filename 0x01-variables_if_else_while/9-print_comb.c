bc1f7239350b03f16e86132945383efcff1538da
meliance <melakmu66@gmail.com>
2023-10-07 07:51:57 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: 'check for the number is positive or negative'
+ *Return: always 0
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
