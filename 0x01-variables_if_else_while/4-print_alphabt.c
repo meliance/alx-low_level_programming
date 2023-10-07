3dd9ee9b62fe17a5adc05ac49692138a43b70eb7
meliance <melakmu66@gmail.com>
2023-10-07 07:00:52 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: 'check for the number is positive or negative'
+ *Return: always 0
+ */
+int main(void)
+{
+	char ab = 'a';
+
+	while (ab <= 'z')
+	{
+		if (ab != 'e' && ab != 'q')
+		{
+		putchar(ab);
+		ab++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
