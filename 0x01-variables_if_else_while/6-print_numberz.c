07454d65614d4c52bb5b33f0fedd9a89eaf2d6bf
meliance <melakmu66@gmail.com>
2023-10-07 07:22:55 -0700

    Write a program that prints all single digit numbers of base 10 starting from 0,You are not allowed to use any variable of type char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: 'check for the number is positive or negative'
+ *Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
