570918ea19228fad2aa2112717257d6a07231adb
meliance <melakmu66@gmail.com>
2023-10-10 12:57:39 -0700

    Write a function that prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,39 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written.
+ * On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * print_alphabet - Prints the lowercase alphabet
- *Description: This function prints all the lowercase letters of the alphabet.
- *Return: void
+ *
+ * Description: This function prints all the lowercase letters of the alphabet.
+ *
+ * Return: None
  */
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-	putchar(letter);
-	}
-	putchar('\n');
+	_putchar(letter);
+	_putchar('\n');
 }
+
 /**
  * main - Entry point of the program
  *
  * Description: This function is the entry point of the program. It calls the
- *              print_alphabet function and returns 0.
+ * print_alphabet function and returns 0.
  *
  * Return: 0
  */
