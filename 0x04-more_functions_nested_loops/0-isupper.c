7d3dd1ced12c99f43d85fdbfcb47c8993b4270b7
meliance <melakmu66@gmail.com>
2023-10-13 05:53:48 -0700

    updated

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,6 @@
 
 /**
  * _isupper - uppercase letters
- * @c: character to check
- *
  * Return: 0 or 1
  */
 int _isupper(int c)
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-* owned by Bwave ICT
+*print_numbers - print 0-9
 *Return: void
 */
 
