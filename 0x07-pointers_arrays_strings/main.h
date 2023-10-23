677ea5967e8dc6e0d10c0b98c409c015c9a7ad07
meliance <melakmu66@gmail.com>
2023-10-23 13:14:16 -0700

    prints the chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,6 +11,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-_putchar(char c);
+int _putchar(char c);
 
 #endif
