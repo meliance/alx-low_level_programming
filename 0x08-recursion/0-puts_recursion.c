98843dc3ca0451a80c1eecc043006f795f6ddbf3
meliance <melakmu66@gmail.com>
2023-10-25 12:36:38 -0700

    a function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == 0)
 	{
-		_putchar(*s);
-		_putchar(s + 1);
+		_putchar('\n');
+		return;
 	}
-	if (*s == '\0')
-	_putchar('\n');
-	return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
