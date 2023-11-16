6fca510e2cbbf63098b89386f4d1bec5df2c25d7
meliance <melakmu66@gmail.com>
2023-11-16 14:30:15 -0800

    a function that returns the number of elements in a linked list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,13 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t noelem;
+	size_t noelem;
 
-    noelem = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        noelem++;
-    }
-    return (noelem);
+	noelem = 0;
+	while (h != NULL)
+	{
+	h = h->next;
+	noelem++;
+	}
+	return (noelem);
 }
