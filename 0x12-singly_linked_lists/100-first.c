36801c4fc81e54329518a2d4f1a4d24303bd2d3a
meliance <melakmu66@gmail.com>
2023-11-16 14:13:49 -0800

    a function that frees a list_t list.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
-
 /**
- * first - prints a sentence before the main
- * function is executed
+ * bmain - function executed before main
+ * Return: no return.
  */
-void first(void)
+
+void __attribute__((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow");
+    printf(",\nI bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,12 @@
 
 void free_list(list_t *head)
 {
-    list_t *current;
+	list_t *current;
 
-    while ((current = head) != NULL)
-    {
-        head = head->next;
-        free(current->str);
-        free(current);
-    }
+	while ((current = head) != NULL)
+	{
+	head = head->next;
+	free(current->str);
+	free(current);
+	}
 }
