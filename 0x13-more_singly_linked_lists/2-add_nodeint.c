120474177af987d9dc51df574763a56811006a75
meliance <melakmu66@gmail.com>
2023-11-20 13:13:37 -0800

    add element at the begning of linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,14 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
+ *
+ * Return: pointer to the new node, or NULL if it fails
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	added = malloc(sizeof(listint_t));
-	if (!added)
-	return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
-	added->n = n;
-	added->next = *head;
-	*head = added;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
-	return (added);
+	return (new);
 }
